be12feec3776ae2dabd11084da5dad6a10794c9a
Luka L <luka.levac@gmail.com>
2021-03-25 20:01:49 +0100

    First test

---
+++ test/TruffleTest1.js
@@ -0,0 +1,154 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+const functions = require("./functions.js");
+
+const {
+    BN,
+    constants,
+    shouldFail,
+    expectRevert
+} = require('@openzeppelin/test-helpers');
+
+const { assert } = require('chai');
+
+const colors = require('colors');
+
+//Contacts
+const contractMarketFactory = artifacts.require("./MarketFactory.sol");
+const contractMarket = artifacts.require("./Market.sol");
+const contractToken = artifacts.require("./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol");
+const contractUniswapV2Factory = artifacts.require("../node_modules/@uniswap/v2-core/contracts/UniswapV2Factory.sol");
+const conatractUniswapV2Pair = artifacts.require("./node_modules/@uniswap/v2-core/contracts/UniswapV2Pair.sol");
+const contractUniswapV2Router02 = artifacts.require("./node_modules/@uniswap/v2-periphery/contracts/UniswapV2Router02");
+
+//Rinkeby contract addresses
+let addressUniswapV2Factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+let addressUniswapV2Pair = "0x8B22F85d0c844Cf793690F6D9DFE9F11Ddb35449";
+let addressTokenDAI = "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735";
+let addressTokenWETH = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
+let addressUniswapV2Router02 = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+
+const startTimestamp = Date.now() / 1000 | 0;
+console.log(startTimestamp);
+
+n=3;
+
+var FrameNextKey;
+
+contract("HorizonTest", async accounts => {
+    it('Should load contracts from deployed addresses', async function() {
+        this.MarketFactory = await contractMarketFactory.new(addressUniswapV2Factory);
+        this.DAIcoin = await contractToken.at(addressTokenDAI);
+        this.UniswapV2Factory = await contractUniswapV2Factory.at(addressUniswapV2Factory);
+        this.UniswapV2Pair = await conatractUniswapV2Pair.at(addressUniswapV2Pair);
+        this.UniswapV2Router02 = await contractUniswapV2Router02.at(addressUniswapV2Router02);
+    });
+    it('Should swap ETH for DAI on ' + n + ' accounts', async function() {
+        for (let i = 0; i < n; i++) {
+            await this.UniswapV2Router02.swapExactETHForTokens(0, [addressTokenWETH, addressTokenDAI], accounts[i], parseInt(+new Date() / 1000) + 3600 * 24, {from: accounts[i], value: new BN('1e18')});
+        }
+    });
+    it('Should add a new market', async function() {
+        await this.MarketFactory.addMarket(this.DAIcoin.address, addressUniswapV2Pair, 86400, 1616635434, 43200, 0, 30, 0, {from: accounts[0]});
+    });
+    it('Should approve market to spend tokens', async function() {
+        var marketKey = await this.MarketFactory.marketsKeys(0);
+        var address = await this.MarketFactory.markets(marketKey);
+        this.market = await contractMarket.at(address);
+
+        for (let i = 0; i < 3; i++) {
+           let tx = await this.DAIcoin.approve(this.market.address, new BN('100e18'), {from: accounts[i]});
+       }
+    });
+    it('Each account should place a wager', async function() {
+        FrameNextKey = await this.market.clcFrameTimestamp((Date.now() / 1000 | 0)+90000);
+        console.log(FrameNextKey);
+         for (let i = 0; i < n; i++) {
+            let tx = await this.market.placeWager(FrameNextKey, 75 - i * 25, 125 + i * 25, {from: accounts[i]});
+         }
+    });
+    it('Mine and report prices', async function() {
+    for(let i=0;i<52;i++){
+        let blockNum = await web3.eth.getBlockNumber();
+        let blockInfo = await web3.eth.getBlock(blockNum);
+        console.log(("Block number: "+blockNum+" timestamp: "+blockInfo.timestamp).bgBlue);
+        //--------------------------------------------------------------
+        await functions.advanceTimeAndBlock(1800*i);
+        //--------------------------------------------------------------
+        let b2 = await web3.eth.getBlockNumber();
+        let t2 = await web3.eth.getBlock(b2);
+        console.log(("Mining: SKIP TIME: " + (t2.timestamp - blockInfo.timestamp) / 3600 + " hours").bgMagenta);
+        console.log(("Block number: "+b2+" timestamp: " + t2.timestamp).bgGreen);
+        console.log(web3.utils.hexToNumber(await this.market.clcFrameTimestamp(t2.timestamp)));
+        //--------------------------------------------------------------
+        //Buy fake DAI so the price changes
+        await this.UniswapV2Router02.swapExactETHForTokens(0, [addressTokenWETH, addressTokenDAI], accounts[0], 1616408752*3600*240, {from: accounts[0], value: new BN('1e18')});
+        await this.market.updateFramePrices({from: accounts[0]});
+    }
+    });
+    it('Close frame and check frame status', async function() {
+        //Close frame
+        let f = await this.market.closeFrame(FrameNextKey, {from: accounts[0]});
+
+        //Check frame status
+        let frame = await this.market.frames(FrameNextKey);
+
+        console.log("Price cumulative start: "+frame.oraclePrice0CumulativeStart);
+        console.log("Timestamp cumulative start: "+frame.oracleTimestampStart);
+        console.log("Price cumulative end: "+frame.oraclePrice0CumulativeEnd);
+        console.log("Timestamp cumulative end: "+frame.oracleTimestampEnd);
+        console.log("Average price : "+ frame.priceAverage);
+        let r= await this.UniswapV2Pair.getReserves();
+        let price = (new BN(r[1])) / (new BN(r[0]));
+        console.log("spot price: "+ price)
+
+        if (frame.state == 2) {
+          console.log("Frame " + FrameNextKey + " in state CLOSED");
+        } else {
+          console.log(colors.red("Frame " + FrameNextKey + " in wrong state "));
+          console.log(colors.red("Frame state is " + frame.state));
+        }
+    }); //TODO: Settle wager and collect fees
+/*    it('Settle wager', async function() {
+        let n = [];
+        //Get all wagers
+        let wagerCount = parseInt(await this.market.getWagersCount());
+        console.log(wagerCount);
+        for (let i = 0; i < wagerCount; i++) {
+            let w = await this.market.wagers(i);
+            if (w.frameKey == FrameNextKey) n.push(i);
+        }
+        let wagersKeys = n;
+        console.log(wagersKeys)
+        let length = this.market.getWagersCount();
+        console.log(length);
+        for (let i = 0; i < length; i++) {
+            //Check shares
+            let shareAmount = await this.market.clcShareAmount(wagersKeys[i]);
+            console.log("Wager " + wagersKeys[i] + " share amount = " + parseFloat(shareAmount) / scalar);
+            let shareHorizon = await this.market.clcShareHorizon(wagersKeys[i]);
+            console.log("Wager " + wagersKeys[i] + " share horizon = " + parseFloat(shareHorizon) / scalar);
+            let shareRange = await this.market.clcShareRange(wagersKeys[i]);
+            console.log("Wager " + wagersKeys[i] + " share range = " + parseFloat(shareRange) / scalar);
+            //Total share
+            let share = await this.market.clcShare(wagersKeys[i]);
+            console.log("Wager " + key + " total share = " + parseFloat(share) / scalar);
+            //Total amount
+            let payoutAmount = await this.market.clcSettleAmount(wagersKeys[i]);
+            console.log("Wager " + key + " payout = " + web3.utils.toBN(payoutAmount).toString());
+            //Settle wager
+            //Get amount of tokens before
+            let wager = await this.market.wagers(wagersKeys[i]);
+            let b1 = web3.utils.toBN(await this.DAIcoin.balanceOf(this.market.address));
+            console.log(colors.yellow("Balance of market is: " + b1));
+            tx = await this.market.settleWager(wagersKeys[i], {from: accounts[i]});
+            let b2 = web3.utils.toBN(await this.DAIcoin.balanceOf(this.market.address));
+            console.log(colors.yellow("Balance of market is: " + b2.toString()));
+            console.log(colors.yellow("Difference is: " + (b1.sub(b2)).toString()));
+            }
+    });*/
+
+
+
+});
