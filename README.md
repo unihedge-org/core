e89919336bba4068807556968808044cb8bdea1f
Luka L <luka.levac@gmail.com>
2021-07-01 12:26:41 +0200

    Uploaded market contract to rinkeby

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-### Rinkeby ETH/DAI market address: 
+### Rinkeby ETH/DAI market address: 0x583e49e09Eb328F13654FB360ec1040627df17ca [link](https://rinkeby.etherscan.io/address/0x583e49e09Eb328F13654FB360ec1040627df17ca#code)
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,17 @@
-let MarketFactory = artifacts.require("MarketFactory");
-let Token = artifacts.require("Token");
-let addrUniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+let MarketFactory = artifacts.require("MarketFactory_3");
 
-const fs = require('fs');
-let contracts = {};
+let addressUniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+let addressUniswapV2Pair = "0x8B22F85d0c844Cf793690F6D9DFE9F11Ddb35449";
+let addressDAIToken = "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735";
+
+
 
 module.exports = async function (deployer) {
     //Deploy MarketFactory
-    //await deployer.deploy(MarketFactory, addrUniswapFactory);
-    //Deploy Token
-    //await deployer.deploy(Token);
-    //
-    // //Write to file contract addresses
-    // contracts["MarketFactory"] = MarketFactory.address;
-    // contracts["Token"] = Token.address;
-    // fs.writeFile("./test/contracts.json", JSON.stringify(contracts), () => {
-    // });
-
+    await deployer.deploy(MarketFactory, addressUniswapFactory);
+    let marketFactory = await MarketFactory.deployed();
+    //Create Market for ETH/DAI with DAI 6h
+    await marketFactory.addMarket(addressDAIToken, addressUniswapV2Pair, 86400, 1625134135, 100, 100, 10000000);
+    let market = await marketFactory.marketsKeys.call(0);
+    console.log("Market address:" + market);
 };
