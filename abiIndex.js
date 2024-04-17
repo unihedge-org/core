657871e022e14039bc3ec93f59084b2f59be1f35
Luka Levac <luka.levac@gmail.com>
2024-04-17 19:50:57 +0200

    path updated

--- abiIndex.js
+++ abiIndex.js
@@ -1,5 +1,8 @@
-import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Function to read and parse a JSON file to get the ABI
 function getContractABI(filePath) {
