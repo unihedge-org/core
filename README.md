9c4059e3e6a1952134269d75265fc1808d05dd5a
Luka Levac <luka.levac@gmail.com>
2025-07-09 20:55:07 +0200

    minimal reward update in getter

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 * Market address: [0x203b6eF28cB1798e674f92422C8f0CD21ebf1Cd3](https://polygonscan.com/address/0x203b6eF28cB1798e674f92422C8f0CD21ebf1Cd3)
     * Accounting Token - USDC: [0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359](https://polygonscan.com/address/0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359)
 
-* Market Getter address: [0xceED16380b4a3913eDA8f1Fab25bB8461E04C320](https://polygonscan.com/address/0xceED16380b4a3913eDA8f1Fab25bB8461E04C320)
+* Market Getter address: [0xECa7ae800672FBa40dC7A4D8504591b130eE52d1](https://polygonscan.com/address/0xECa7ae800672FBa40dC7A4D8504591b130eE52d1)
 
 * Claim Verifier address: [0xEDdff728784312214d9d68fEdDEf4079b1968a63](https://polygonscan.com/address/0xEDdff728784312214d9d68fEdDEf4079b1968a63)
--- contracts/MarketGetter.sol
+++ contracts/MarketGetter.sol
@@ -480,21 +480,26 @@ contract MarketGetter {
     /// @notice Remaining tax of a lot owner get's returned after a sale (change of ownership). So the award amount isn't for sure untill the frame get's closed.
     /// @param frameKey Market.Frame's timestamp
     /// @return rewardFund amount 
-    function getRewardAmountMin(Market market, uint frameKey) external view returns (uint rewardFund) {     
+    /// @notice Get minimum guaranteed frame's award amount (accumulation of lot taxes)
+    function getRewardAmountMin(Market market, uint frameKey) external view returns (uint rewardFund) {
         Market.Frame memory frame = getFrameStruct(market, frameKey);
-        // Get the index of this frame in the framesKeys array
         uint frameIndex = market.getFrameIndex(frameKey);
+
+        uint totalReward;
         if (frameIndex > 0) {
-            // Get the previous frame's reward pool
             Market.Frame memory prevFrame = getFrameStruct(market, market.framesKeys(frameIndex - 1));
-            // Calculate the reward fund as the sum of the previous frame's reward pool and the current frame's reward pool
-            rewardFund = (prevFrame.rewardPool / 2) + frame.rewardPool;
+            totalReward = ((prevFrame.rewardPool / 2) + frame.rewardPool) / 2;
         } else {
-            // If this is the first frame, the reward fund is just the current frame's reward pool
-            rewardFund = frame.rewardPool;
+            totalReward =  (frame.rewardPool / 2);
         }
+
+        // Correct Q96-based fee calculation
+        uint fee = mulDiv(totalReward, market.feeProtocol(), FixedPoint96.Q96);
+        rewardFund = totalReward - fee;
+
         return rewardFund;
     }
+
 
     /// @notice Calculate amount required to approve to buy a lot
     /// @param timestamp Timestamp that determins which frame it is
@@ -529,6 +534,103 @@ contract MarketGetter {
         uint tax2 = market.clcTax(frameKey, acqPrice);
         if(tax1 >= tax2) amnt = 0;
         else amnt = tax2-tax1;
+    }
+
+        // Uniswap V3 mulDiv function
+    // This function is used to multiply two numbers and then divide by a third number, all in a single operation.
+    // It's converted to solidity v.0.8.0 from the original Uniswap V3 code.
+   function mulDiv(
+        uint256 a,
+        uint256 b,
+        uint256 denominator
+    ) internal pure returns (uint256 result) {
+        // 512-bit multiply [prod1 prod0] = a * b
+        // Compute the product mod 2**256 and mod 2**256 - 1
+        // then use the Chinese Remainder Theorem to reconstruct
+        // the 512 bit result. The result is stored in two 256
+        // variables such that product = prod1 * 2**256 + prod0
+        uint256 prod0; // Least significant 256 bits of the product
+        uint256 prod1; // Most significant 256 bits of the product
+        assembly {
+            let mm := mulmod(a, b, not(0))
+            prod0 := mul(a, b)
+            prod1 := sub(sub(mm, prod0), lt(mm, prod0))
+        }
+
+        // Handle non-overflow cases, 256 by 256 division
+        if (prod1 == 0) {
+            require(denominator > 0);
+            assembly {
+                result := div(prod0, denominator)
+            }
+            return result;
+        }
+
+        // Make sure the result is less than 2**256.
+        // Also prevents denominator == 0
+        require(denominator > prod1);
+
+        ///////////////////////////////////////////////
+        // 512 by 256 division.
+        ///////////////////////////////////////////////
+
+        // Make division exact by subtracting the remainder from [prod1 prod0]
+        // Compute remainder using mulmod
+        uint256 remainder;
+        assembly {
+            remainder := mulmod(a, b, denominator)
+        }
+        // Subtract 256 bit number from 512 bit number
+        assembly {
+            prod1 := sub(prod1, gt(remainder, prod0))
+            prod0 := sub(prod0, remainder)
+        }
+
+        // Factor powers of two out of denominator
+        // Compute largest power of two divisor of denominator.
+        // Always >= 1.
+        uint256 twos = (~denominator + 1) & denominator;
+        // Divide denominator by power of two
+        assembly {
+            denominator := div(denominator, twos)
+        }
+
+        // Divide [prod1 prod0] by the factors of two
+        assembly {
+            prod0 := div(prod0, twos)
+        }
+        // Shift in bits from prod1 into prod0. For this we need
+        // to flip `twos` such that it is 2**256 / twos.
+        // If twos is zero, then it becomes one
+        assembly {
+            twos := add(div(sub(0, twos), twos), 1)
+        }
+        prod0 |= prod1 * twos;
+
+        // Invert denominator mod 2**256
+        // Now that denominator is an odd number, it has an inverse
+        // modulo 2**256 such that denominator * inv = 1 mod 2**256.
+        // Compute the inverse by starting with a seed that is correct
+        // correct for four bits. That is, denominator * inv = 1 mod 2**4
+        uint256 inv = (3 * denominator) ^ 2;
+        // Now use Newton-Raphson iteration to improve the precision.
+        // Thanks to Hensel's lifting lemma, this also works in modular
+        // arithmetic, doubling the correct bits in each step.
+        inv *= 2 - denominator * inv; // inverse mod 2**8
+        inv *= 2 - denominator * inv; // inverse mod 2**16
+        inv *= 2 - denominator * inv; // inverse mod 2**32
+        inv *= 2 - denominator * inv; // inverse mod 2**64
+        inv *= 2 - denominator * inv; // inverse mod 2**128
+        inv *= 2 - denominator * inv; // inverse mod 2**256
+
+        // Because the division is now exact we can divide by multiplying
+        // with the modular inverse of denominator. This will give us the
+        // correct result modulo 2**256. Since the precoditions guarantee
+        // that the outcome is less than 2**256, this is the final result.
+        // We don't need to compute the high bits of the result and prod1
+        // is no longer required.
+        result = prod0 * inv;
+        return result;
     }
 
 
